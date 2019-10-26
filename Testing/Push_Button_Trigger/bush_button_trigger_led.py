8462a925e4eb04e08848273d6f1059dded450062
Hagen E. Fritz <hagenfritz@gmail.com>
2019-10-26 09:21:44 -0500

    created test code for push button:

---
+++ Testing/Push_Button_Trigger/bush_button_trigger_led.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 09:00:58 2019
+
+@author: hagenfritz
+"""
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+# LED
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.output(17,False)
+# Push Button
+GPIO.setup(27,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(27,GPIO.RISING,bouncetime=200)
+
+try:
+    while True:
+        if GPIO.event_detected(27):
+            activate = True
+            while activate is True:
+                GPIO.output(17,True)
+                sleep(0.5)
+                GPIO.output(17,False)
+                sleep(0.5)
+                if GPIO.event_detected(27):
+                    activate = False
+        else:
+            GPIO.output(17,False)
+            
+except KeyboardInterrupt:
+    GPIO.cleanup()
