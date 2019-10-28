f214368f7b7e740afcafc0de16558f5f665095ad
Hagen E. Fritz <hagenfritz@gmail.com>
2019-10-27 19:36:57 -0500

    created servo changes

--- Testing/Temperature_Trigger/temperature_trigger.py
+++ Testing/Temperature_Trigger/temperature_trigger.py
@@ -12,6 +12,10 @@ import board
 import busio
 import adafruit_sht31d
 
+# ----- #
+# Setup #
+# --------------------------------------------------------------------------- #
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 # red LED
@@ -23,6 +27,19 @@ GPIO.output(17,False)
 # T/RH sensor
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
+# SG90 Servo
+## Specifics to the servo
+cooling_duty = 2.2
+heating_duty = 12.4
+center = (heating_duty+cooling_duty)/2
+## Pin setup
+servo_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+## Create PWM channel on the servo pin with a frequency of 50Hz
+pwm_servo = GPIO.PWM(servo_pin, 50)
+pwm_servo.start(center)
+
 
 try:
     while True:
@@ -30,11 +47,14 @@ try:
         print('Temperature',tc)
         if tc > 22: # if the button has been pushed
             GPIO.output(17,True)
+            pwm_servo.ChangeDutyCycle(heating_duty)
         elif tc < 20:
             GPIO.output(27,True)
+            pwm_servo.ChangeDutyCycle(cooling_duty)
         else:
             GPIO.output(17,False)
             GPIO.output(27,False)
+            pwm_servo.ChangeDutyCycle(center)
         
         sleep(5)
             
