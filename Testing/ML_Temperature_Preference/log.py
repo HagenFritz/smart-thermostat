57ad6f51c91d1888c25e115376216c67dd429fc6
Hagen_Fritz <33231914+HagenFritz@users.noreply.github.com>
2019-12-27 13:21:40 -0600

    created

---
+++ Testing/ML_Temperature_Preference/log.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 09:00:58 2019
+@author: hagenfritz
+"""
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+# LED
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.output(17,False)
+# Push Button
+GPIO.setup(27,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(27,GPIO.RISING,bouncetime=200)
+
+try:
+    while True:
+        if GPIO.event_detected(27): # if the button has been pushed
+            activate = True
+            while activate is True:
+                # Light flashes
+                GPIO.output(17,True)
+                sleep(0.5)
+                GPIO.output(17,False)
+                sleep(0.5)
+                if GPIO.event_detected(27):
+                    activate = False
+        else:
+            GPIO.output(17,False)
+            
+except KeyboardInterrupt:
+    # resets GPIOs
+    GPIO.cleanup()
