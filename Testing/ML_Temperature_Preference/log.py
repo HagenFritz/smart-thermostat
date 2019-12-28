6a383aa4cbf588976693c4873fa3cb4f60dc5366
Hagen E. Fritz <hagenfritz@gmail.com>
2019-12-27 23:48:30 -0600

    updated the lightining convention

--- Testing/ML_Temperature_Preference/log.py
+++ Testing/ML_Temperature_Preference/log.py
@@ -8,6 +8,7 @@ Created on Sat Oct 26 09:00:58 2019
 import RPi.GPIO as GPIO
 import time
 from datetime import datetime
+from random import randint
 
 def led_flash(pin):
 	'''
@@ -20,6 +21,12 @@ def led_flash(pin):
 	time.sleep(sleep_time)
 	GPIO.output(pin,GPIO.HIGH)
 	time.sleep(sleep_time)
+
+def light(colors):
+	'''
+	Chooses a light to turn on
+	'''
+	num = randint(0,3)
 
 GPIO.setmode(GPIO.BCM)
 
@@ -36,21 +43,20 @@ for pin in button_pins:
 	GPIO.setup(pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 	GPIO.add_event_detect(pin,GPIO.RISING,bouncetime=200)
 
-color_pins = [26,16,6,5,25]
+color_pins = {'white':26,'blue':16,'green':6,'yellow':5,'red':25}
 
 try:
 	while True:
-		if datetime.now().hour < 4:
-			GPIO.output(color_pins[0],GPIO.LOW)
-		elif datetime.now().hour < 8:
-			GPIO.output(color_pins[1],GPIO.LOW)
-		elif datetime.now().hour < 12:
-			GPIO.output(color_pins[2],GPIO.LOW)
-		elif datetime.now().hour < 16:
-			led_flash(color_pins[3])
-			GPIO.output(color_pins[3],GPIO.LOW)
+		if datetime.now().hour % 2 == 0:
+			GPIO.output(color_pins['blue'],GPIO.LOW)
+		elif datetime.now().hour % 3 == 0:
+			GPIO.output(color_pins['green'],GPIO.LOW)
+		elif datetime.now().hour % 5 == 0:
+			GPIO.output(color_pins['yellow'],GPIO.LOW)
+		elif datetime.now().hour % 7 == 0:
+			GPIO.output(color_pins['green'],GPIO.LOW)
 		else:
-			GPIO.output(color_pins[4],GPIO.LOW)
+			GPIO.output(color_pins['white'],GPIO.LOW)
 
 		# Red Button
 		if GPIO.event_detected(button_pins[0]):
